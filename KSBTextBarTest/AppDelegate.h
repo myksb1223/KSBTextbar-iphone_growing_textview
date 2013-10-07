df532fa148a368e3ae37380b6cffd71d24b7144b
Seungbeom Kim <myksb1223@Seungbeom-Kim-ui-MacBook-Air.local>
2013-10-07 23:22:31 +0900

    Initial Commit

---
+++ KSBTextBarTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KSBTextBarTest
+//
+//  Created by Seungbeom Kim on 13. 10. 7..
+//  Copyright (c) 2013년 Seungbeom Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KSBTextBarTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KSBTextBarTest
+//
+//  Created by Seungbeom Kim on 13. 10. 7..
+//  Copyright (c) 2013년 Seungbeom Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
